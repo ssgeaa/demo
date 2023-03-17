42ca022099b276f88221acfb872bb042ff90dd75
ssgeaa <aderangga@student.uns.ac.id>
2023-03-17 15:48:47 +0700

    pusing wak yg ini

---
+++ 17-maret.py
@@ -0,0 +1,43 @@
+'''print("CHALLENGE 2")
+
+mata_uang = {
+    'IDR' : 1,
+    'USD' : 0.000071
+}
+
+uang = input("masukkan mata uang yang ingin dikonversi (IDR, USD): ")
+nilai = int(input("masukkan nilai uang: "))
+
+print(mata_uang[uang]*nilai)
+
+print("")
+
+print("CHALLENGE 3")
+
+classroom = {
+    'praktikan': {
+        'row1': {
+            'anggota1': {
+                'name': 'Dimas'
+                'age': 19,
+            },
+            'anggota2': {
+                'name': 'Fiki'
+                'age': 19,
+            },
+            'anggota3': {
+                'name': 'Deo'
+                'age': 19,
+            },
+        },
+    },
+},'''
+
+string = "ikan bakar gosong item pait banget"
+print(string[0:4])
+print(string[5:17])
+print(string[18:21])
+print(string[5:25])
+
+print("gatau yaallah aku pusing mau bobo")
+print("smgt yaaahhh remed up")
